65f7b6694adbf348eb99fce078e1fe548764d9e2
Zion Tuiasoa <ztuiasoa@icloud.com>
2023-02-21 14:54:47 -0700

    Initial Commit

---
+++ Pompom/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Pompom
+//
+//  Created by Zion Tuiasoa on 2/21/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    let tasks = ["Code", "Feed the squirrels", "Drink so much coffee", "Play guitar"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
